66436c4ddde8a6f7b0144887b09462dd6cdd9cb4
lukem <lukemelaia.personal@gmail.com>
2023-08-09 04:43:09 +0200

    Added URI's to pages.

--- src/main/java/org/dynasite/Page.java
+++ src/main/java/org/dynasite/Page.java
@@ -2,6 +2,8 @@ package org.dynasite;
 
 import fi.iki.elonen.NanoHTTPD;
 
+import java.net.URI;
+
 /**
  * Basic implementable representation of a webpage or other web content
  * which is served to a client upon request, usually at specific urls.
@@ -10,10 +12,19 @@ import fi.iki.elonen.NanoHTTPD;
  */
 public abstract class Page {
 
+    private final URI uri;
+
+    protected Page(URI uri) {
+        this.uri = uri;
+    }
+
     public NanoHTTPD.Response getPageResponse() {
         return NanoHTTPD.newFixedLengthResponse(
                 NanoHTTPD.Response.Status.OK, NanoHTTPD.MIME_HTML, "<h1>Default Dynasite Page</h1>"
         );
     }
 
+    public URI getUri() {
+        return uri;
+    }
 }
