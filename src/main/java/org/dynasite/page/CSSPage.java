552e1da8a1c3eeda2a19cafe9cf48ad4933046af
lukem <lukemelaia.personal@gmail.com>
2023-09-10 08:34:20 +0200

    Added CSS Pages.

---
+++ src/main/java/org/dynasite/page/CSSPage.java
@@ -0,0 +1,42 @@
+package org.dynasite.page;
+
+import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.NanoHTTPD.Response;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.Objects;
+
+public class CSSPage extends Page {
+
+    // Factory Methods
+
+    public static CSSPage getFromFile(File file) throws IOException {
+        return new CSSPage(new String(Files.readAllBytes(file.toPath())));
+    }
+
+    public static CSSPage getFromResource(String path) throws IOException {
+        return new CSSPage(new String(Objects.requireNonNull(CSSPage.class.getResourceAsStream(path)).readAllBytes()));
+    }
+
+    // Class
+
+    public static final String MIME_CSS = "text/css";
+
+    protected String css;
+
+    public CSSPage(String css) {
+        this.css = Objects.requireNonNull(css);
+    }
+
+    @Override
+    public Response getPageResponse(Map<String, String> headers, NanoHTTPD.IHTTPSession session) {
+        return getCSSResponse(css);
+    }
+
+    protected Response getCSSResponse(String CSS) {
+        return NanoHTTPD.newFixedLengthResponse(Response.Status.OK, MIME_CSS, CSS);
+    }
+}
