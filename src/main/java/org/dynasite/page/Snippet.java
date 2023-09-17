73657acbd8183be8988fcc497145eb1ccc7cebce
lukem <lukemelaia.personal@gmail.com>
2023-09-17 04:07:03 +0200

    Documented Snippets and the Template Exception Class.

--- src/main/java/org/dynasite/page/Snippet.java
+++ src/main/java/org/dynasite/page/Snippet.java
@@ -5,18 +5,31 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Objects;
 
+/**
+ * Represents basic text snippets contained within files,
+ * either on disk or in the jar. Which can be inserted into
+ * {@link Page} responses.
+ */
 public class Snippet {
 
     private final String snippetName;
 
     private String snippetText = null;
 
+    /**
+     * Creates a new named snippet.
+     *
+     * @param snippetName the snippet name.
+     */
     public Snippet(String snippetName) {
         this.snippetName = Objects.requireNonNull(snippetName);
     }
 
     // Getters
 
+    /**
+     * @return the text of the snippet (read from file).
+     */
     public String get() {
         if(this.snippetText == null) {
             throw new SnippetLoadException(this, "Snippet has not been loaded.");
@@ -25,6 +38,9 @@ public class Snippet {
         return this.snippetText;
     }
 
+    /**
+     * @return the snippets name.
+     */
     public String getName() {
         return snippetName;
     }
@@ -39,6 +55,13 @@ public class Snippet {
 
     // Loaders
 
+    /**
+     * Loads the snippet text from a file. Cannot be called multiple times.
+     *
+     * @param file the file to load the text from.
+     * @return the loaded snippet.
+     * @throws SnippetLoadException if the file cannot be read or the snippet cannot be loaded.
+     */
     public Snippet loadFromFile(File file) {
         try {
             setSnippetText(new String(Files.readAllBytes(file.toPath())));
@@ -49,6 +72,13 @@ public class Snippet {
         return this;
     }
 
+    /**
+     * Loads the snippet text from a file within the jar. Cannot be called multiple times.
+     *
+     * @param resource the resource location to load the file/text from.
+     * @return the loaded snippet.
+     * @throws SnippetLoadException if the file cannot be read or the snippet cannot be loaded.
+     */
     public Snippet loadFromResource(String resource) {
         try {
             setSnippetText(new String(
@@ -63,6 +93,9 @@ public class Snippet {
 
     // Snippet Exception
 
+    /**
+     * Thrown when an error is raised working with, assigning, creating, or accessing {@link Snippet Snippets}.
+     */
     public static class SnippetLoadException extends RuntimeException {
 
         protected SnippetLoadException(Snippet snippet, String message) {
--- src/main/java/org/dynasite/page/TemplateNotFoundException.java
+++ src/main/java/org/dynasite/page/TemplateNotFoundException.java
@@ -1,6 +1,16 @@
 package org.dynasite.page;
 
+/**
+ * Thrown when an error occurs creating a Template from a template file
+ * which cannot be found.
+ *
+ * @see HTMLPageTemplate
+ * @see JSPageTemplate
+ * @see JSONPageTemplate
+ * @see CSSPageTemplate
+ */
 public class TemplateNotFoundException extends RuntimeException {
+
     public TemplateNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
