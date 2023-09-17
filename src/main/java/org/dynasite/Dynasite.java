80ebcbd572c6371d0f1e10bd0957c7cea668fc88
lukem <lukemelaia.personal@gmail.com>
2023-09-17 04:15:01 +0200

    Documented the Dynasite class.

--- src/main/java/org/dynasite/Dynasite.java
+++ src/main/java/org/dynasite/Dynasite.java
@@ -7,12 +7,19 @@ import java.io.IOException;
 import java.util.Objects;
 
 /**
- * Main class for hosting a site.
+ * Main class for hosting a site. See tests for simple examples
+ * on the types of sites which can be created.
  */
 public class Dynasite {
 
+    /**
+     * Default hostname the site is hosted on. Can be changed.
+     */
     public static String hostname = "http://localhost";
 
+    /**
+     * Default response timeout (1 second). Can be changed.
+     */
     public static int timeout = 1000; //Default 1 second timeout
 
     private final int port;
@@ -21,6 +28,11 @@ public class Dynasite {
 
     private final HostServer host;
 
+    /**
+     * Create a new Dynasite to host the given {@link HostServer}.
+     *
+     * @param host server to host.
+     */
     public Dynasite(HostServer host) {
         this.port = host.getPort();
         this.host = Objects.requireNonNull(host);
@@ -28,20 +40,30 @@ public class Dynasite {
 
     }
 
-    @SuppressWarnings("unused")
+    /**
+     * @return the port the site is being hosted on.
+     */
     public int getPort() {
         return port;
     }
 
-    @SuppressWarnings("unused")
+    /**
+     * @return the server implementation being hosted.
+     */
     public Server getServer() {
         return server;
     }
 
+    /**
+     * @return the url where the site is being served.
+     */
     public String getHostURL() {
         return hostname + ":" + this.port;
     }
 
+    /**
+     * Starts and hosts the server as in a separate non-daemon thread.
+     */
     public void start() {
         try {
             host.start(timeout, false);
